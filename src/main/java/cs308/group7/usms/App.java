fb457a24e4bc6d39f8ee609da9ad61ed753bb311
Matthew Liddle SE2021 <matthew.liddle.2021@uni.strath.ac.uk>
2023-10-19 18:34:21 +0100

    Remove SystemOperations.java

--- src/main/java/cs308/group7/usms/App.java
+++ src/main/java/cs308/group7/usms/App.java
@@ -3,11 +3,9 @@ package cs308.group7.usms;
 public class App {
 
     public static DatabaseConnection databaseConnection;
-    public static SystemOperations systemOperations;
 
     public static void main(String[] args) {
         databaseConnection = new DatabaseConnection("src/main/resources/dbConnect.txt");
-        systemOperations = new SystemOperations(databaseConnection);
         System.out.println("Hello world!");
     }
 }
