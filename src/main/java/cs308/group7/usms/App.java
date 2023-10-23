79e31fa2c34df7be418fd2b86ce1feee18759995
Matthew Liddle SE2021 <matthew.liddle.2021@uni.strath.ac.uk>
2023-10-23 16:02:53 +0100

    Implement DatabaseConnection methods - Add dependencies for HikariCP & log4j

--- src/main/java/cs308/group7/usms/App.java
+++ src/main/java/cs308/group7/usms/App.java
@@ -1,11 +1,33 @@
 package cs308.group7.usms;
 
+import javax.sql.rowset.CachedRowSet;
+
 public class App {
 
     public static DatabaseConnection databaseConnection;
 
     public static void main(String[] args) {
-        databaseConnection = new DatabaseConnection("src/main/resources/dbConnect.txt");
-        System.out.println("Hello world!");
+
+        // Create database pool
+        try {
+            databaseConnection = new DatabaseConnection("src/main/resources/dbConnect.txt");
+        } catch (Exception e) {
+            System.out.println("There was an error creating database pool!: " + e);
+            System.exit(65);
+        }
+
+        // Application code (sample for demonstration)
+        try (CachedRowSet res = databaseConnection.select(new String[]{"Course"}, new String[]{"Name", "Description"}, null)) {
+            while (res.next()) {
+                System.out.println(res.getString("Name") + ": " + res.getString("Description"));
+            }
+        } catch (Exception e) {
+            System.out.println("There was an error querying the database!: " + e);
+        }
+
+
+        // Close database pool
+        databaseConnection.close();
+
     }
 }
