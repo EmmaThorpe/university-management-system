036acc5d4cc611b4df2ee1250c4eef0a35caf209
Fiona McLaren <fionamclaren268@gmail.com>
2023-10-23 17:56:58 +0100

    more styles added and replaced layout

--- src/main/java/cs308/group7/usms/ui/loginUI.java
+++ src/main/java/cs308/group7/usms/ui/loginUI.java
@@ -3,11 +3,14 @@ package cs308.group7.usms.ui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class loginUI extends Application {
@@ -17,36 +20,36 @@ public class loginUI extends Application {
         String css = this.getClass().getResource("/style/style.css").toExternalForm();
         scene.getStylesheets().add(css);
         primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
         primaryStage.show();
-
-
     }
 
     public Scene loginScene(Stage primaryStage){
-        Label email=new Label("Email");
-        Label password=new Label("Password");
-        TextField tf1=new TextField();
-        TextField tf2=new TextField();
+
+        GridPane formContent = loginFields();
 
         Button Submit=new Button("Submit");
+        HBox formBtns = new HBox(Submit);
+        Label l = new Label("button not selected");
         Submit.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent arg0) {
-                primaryStage.setScene(signUpScene(tf1.getText().toString()));
+                /*
+                primaryStage.setScene(signUpScene("hello"));
                 primaryStage.show();
+                 */
+                l.setText("selected");
             }
         });
 
-        GridPane root=new GridPane();
-        root.setHgap(20);
-        root.setVgap(15);
-        Scene scene = new Scene(root,500,300);
-        root.addRow(0, email,tf1);
-        root.addRow(1, password,tf2);
-        root.addRow(2, Submit);
+        BorderPane root = new BorderPane(formContent);
 
-        //Adding CSS file to the root
+        formBtns.setPadding(new Insets(20, 0, 0, 0));
+        root.setBottom(formBtns);
+
+        root.setPadding(new Insets(10));
+
+        Scene scene = new Scene(root);
         return scene;
     }
 
@@ -59,4 +62,21 @@ public class loginUI extends Application {
         root.addRow(0, test);
         return scene;
     }
+
+
+    private GridPane loginFields() {
+        Label email=new Label("Email");
+        Label password=new Label("Password");
+        TextField tf1=new TextField();
+        TextField tf2=new TextField();
+
+        GridPane root = new GridPane();
+        root.setHgap(20);
+        root.setVgap(20);
+        root.addRow(0, email, tf1);
+        root.addRow(1, password, tf2);
+        return root;
+    }
 }
+
+
